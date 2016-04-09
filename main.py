40d7d93969cbfe2f7c694d75c131a424eb1717ff
Pierre Rochard <pierre@rochard.org>
2016-04-09 15:52:20 -0400

    refactoring

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 import os
 import subprocess
 from subprocess import PIPE, STDOUT
-from github3 import login
+import platform
 
-from config import token
+from github3 import login
+import keyring
+
+token = keyring.get_password('github', 'token')
 
 gh = login(token=token)
 
+if platform == 'Windows':
+    git_command = 'C:/Users/Pierre/AppData/Local/Programs/Git/bin/git.exe'
+else:
+    git_command = 'git'
+
 
 def clone_repo(repo, starred=False):
-    print(repo.ssh_url)
+    print(repo.html_url)
     print(repo.name)
     repo_directory = os.path.expanduser('~')
     if not starred:
-        repo_directory = os.path.join(repo_directory, 'src', repo.name)
+        repo_directory = os.path.join(repo_directory, 'github-src', repo.name)
     else:
-        repo_directory = os.path.join(repo_directory, 'src', 'starred', repo.name)
+        repo_directory = os.path.join(repo_directory, 'github-src', 'starred', repo.name)
     if not os.path.exists(repo_directory):
-        proc = subprocess.run(['C:/Users/Pierre/AppData/Local/Programs/Git/bin/git.exe',
-                               'clone',
-                               repo.ssh_url,
-                               repo_directory], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        proc = subprocess.run([git_command, 'clone', repo.html_url, repo_directory],
+                              stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         print(proc.stdout.decode())
-    proc = subprocess.run(['C:/Users/Pierre/AppData/Local/Programs/Git/bin/git.exe',
-                           '-C',
-                           repo_directory,
-                           'pull'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    proc = subprocess.run([git_command, '-C', repo_directory, 'pull'],
+                          stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     print(proc.stdout.decode())
 
 
 if __name__ == '__main__':
-    for repo in gh.repositories():
+    for repo in gh.iter_all_repos():
         clone_repo(repo)
     starred = gh.starred()
     for repo in starred:
